27b9228f71ac1624f486a9ff48e4a78ed8893e29
Keegan <jonesk74@uw.edu>
2017-11-26 14:25:44 -0800

    Test for CUDA support
    Just need to include cudart.dll with the application.

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -1,23 +1,25 @@
 // Include modules
 #define USING_CNOISE_NAMESPACES
 #include "modules/Modules.hpp"
+#include "cuda_assert.h"
+#include <cuda_runtime.h>
 #include <iostream>
 
 // Check for CUDA support
 int cuda_supported() {
-    try {
-        
-        return 1;
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Couldn't verify CUDA support: " << e.what() << "\n";
-        return 0;
-    }
+    int result = 0;
+    auto err = cudaGetDeviceCount(&result);
+    return result;
 }
 
 int main() {
+    int cuda_check = cuda_supported();
+    if (cuda_check != 0) {
+        cnoise::Module::CUDA_LOADED = true;
+    }
+    else {
+        cnoise::Module::CUDA_LOADED = false;
+    }
 
-    int img_size_x = 2048;
-    int img_size_y = 1024;
     
 }
